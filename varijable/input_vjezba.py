9569ac3c0e42a9806c56d1e7b2bdd8727e260ad6
Vlaoviks <116379645+Vlaoviks@users.noreply.github.com>
2022-10-25 21:25:29 +0200

    Uvod u varijable i tipove podataka

---
+++ varijable/input_vjezba.py
@@ -0,0 +1,11 @@
+prvi_broj = int(input ("Unesi prvi broj:"))
+drugi_broj = int(input ("Unesi drugi broj:"))
+
+print(type(prvi_broj))
+print(prvi_broj + drugi_broj)
+print(prvi_broj - drugi_broj)
+print(prvi_broj * drugi_broj)
+print(prvi_broj / drugi_broj)
+print(prvi_broj // drugi_broj)
+print(prvi_broj % drugi_broj)
+
---
+++ varijable/print_vjezba.py
@@ -0,0 +1,8 @@
+ime = "Pero"
+godine = 20
+punoljetan = True
+zanimanje = "developer"
+
+print(ime, godine, punoljetan, zanimanje, sep= "\n") # predstavlja skok u novi red sa naredbom "\n"
+print(ime, end=",") # ispisuje se sve do ,
+print(ime, godine, punoljetan, sep=":", end=";")
